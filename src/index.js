7ae4e1f3b26bfbfb1fc5b138ca118f06e7dea0ea
howdyAnkit <palankit35@gmail.com>
2021-02-21 11:23:32 +0530

    Basic webpack Explanation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-const elevenShield = {
-  leatherStrips: 2,
-  ironIngot: 1,
-  refinedMoonStone: 4,
+const webpackBasic = {
+  BasicSetup: true, //file --> webpack.config.js && package.json
+  hotReload: true, // file --> package.json (yarn start)
+  bundlerIntialization: true, //file --> webpack.config.js
 };
 
-console.log(elevenShield);
+const webpackBasicDefination = {
+  BasicSetup:
+    "We are trying to set (start && watch) which is better serve or dev-server? (https://stackoverflow.com/questions/49418818/how-is-webpack-serve-different-from-webpack-dev-server)",
+  hotReload:
+    "We are achieving live reloading of any pages when any content Changes with the help of --watch",
+  ContentBase:
+    "ContentBase Just directs the entryPoint where should the server look for files instead of just showing directory if there are no contentbase providede it will load the directory instead of Page in Browser",
+};
+
+console.log(webpackBasic);
+console.log(webpackBasicDefination.hotReload);
